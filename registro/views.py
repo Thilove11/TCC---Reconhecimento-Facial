520db1454bf44cde4801e36f14d9c04b81ce4eb2
thilove11 <thi11.thilove@gmail.com>
2025-07-24 18:21:58 -0300

    extracao_facial

--- registro/views.py
+++ registro/views.py
@@ -1,3 +1,5 @@
+import cv2
+import os
 from django.shortcuts import render, redirect
 from .forms import FuncionarioForm, ColetaFacesForm
 from .models import Funcionario, ColetaFaces
@@ -27,13 +29,70 @@ def criar_funcionario(request):
         form = FuncionarioForm()
     return render(request, 'criar_funcionario.html', {'form': form})
 
+def extract(camera_detection, funcionario_slug):
+    amostra = 0
+    numeroAmostras = 10
+    largura, altura = 220, 220
+    file_paths = []
+
+    while amostra < numeroAmostras:
+        ret, frame = camera_detection.get_camera()
+        crop = camera_detection.sample_faces(frame)
+
+        if crop is not None:
+            amostra += 1
+
+            face = cv2.resize(crop, (largura, altura))
+            imagemCinza = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+            file_name_path = f'./tmp/{funcionario_slug}_{amostra}.jpg'
+            print(file_name_path)
+
+            cv2.imwrite(file_name_path, imagemCinza)
+            file_paths.append(file_name_path)
+        else:
+            print("Face não encontrada")
+
+        if amostra >= numeroAmostras:
+            break
+
+    camera_detection.restart()
+    return file_paths
+
+def face_extract(context, funcionario):
+    num_coletas = ColetaFaces.objects.filter(funcionario__slug=funcionario.slug).count()
+
+    print(num_coletas)
+    if num_coletas >= 10:
+        context['erro'] = 'Limite máximo de coletas atingido.'
+    else:
+        file_paths = extract(camera_detection, funcionario.slug)
+        print(file_paths)
+
+        for path in file_paths:
+            coleta_face = ColetaFaces.objects.create(funcionario=funcionario)
+            coleta_face.image.save(os.path.basename(path), open(path, 'rb'))
+            os.remove(path)
+
+        context['file_paths'] = ColetaFaces.objects.filter(funcionario__slug=funcionario.slug)
+        context['extracao_ok'] = True
+
+    return context
+
 def criar_coleta_faces(request, funcionario_id):
     print(funcionario_id)
     funcionario = Funcionario.objects.get(id=funcionario_id)
 
+    botao_clicado = request.GET.get('clicked', 'False') == 'True'
     context = {
         'funcionario': funcionario,
         'face_detection': face_detection,
+        'valor_botao': botao_clicado,
     }
 
+    if botao_clicado:
+        print("Cliquei em Extrair Imagens!")
+        context = face_extract(context, funcionario)
+
     return render(request, 'criar_coleta_faces.html', context)
+
